3a5d980efd2daa822fa6611477a391ff90928084
skobow <FattyPro1>
2017-08-07 06:46:32 +0200

    Unit tests

--- src/test/java/net/skobow/spring/data/mongodb/mapping/MappingDocumentRepositoryTest.java
+++ src/test/java/net/skobow/spring/data/mongodb/mapping/MappingDocumentRepositoryTest.java
@@ -1,5 +1,6 @@
 package net.skobow.spring.data.mongodb.mapping;
 
+import net.skobow.spring.data.mongodb.core.Document;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -14,12 +15,25 @@ public class MappingDocumentRepositoryTest {
     @Mock
     private MongoTemplate mongoTemplateMock;
 
+    private DocumentMapper documentMapper = new DocumentMapper<Document, Object>
+    private MappingDocumentRepository documentRepository;
+
     @Before
     public void setUp() throws Exception {
+        MappingContext mappingContext = new MappingContext<>(new Class<DocumentClass>(), new Class<DomainClass>(),);
+        documentRepository = new MappingDocumentRepository(mongoTemplateMock, mappingContext);
     }
 
     @Test
     public void test() {
 
+    }
+    
+    class DocumentClass implements Document {
+        
+    }
+    
+    class DomainClass {
+        
     }
 }
